473003803e7522215b43e45c6817e252c819bf7d
sushenman <sushenman83@gmail.com>
2023-03-18 09:50:44 +0545

    Initial Commit

---
+++ app/src/main/java/com/example/wordsapp/WordAdapter.java
@@ -0,0 +1,65 @@
+package com.example.wordsapp;
+import static androidx.core.content.ContextCompat.startActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class WordAdapter extends RecyclerView.Adapter<WordAdapter.WordViewHolder> {
+
+    Context context;
+    ArrayList<WordModel> modelArrayList;
+
+    public WordAdapter(Context context, ArrayList<WordModel> modelArrayList) {
+        this.context = context;
+        this.modelArrayList = modelArrayList;
+    }
+
+    @NonNull
+    @Override
+    public WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater =LayoutInflater.from(parent.getContext());
+        View view =layoutInflater.inflate(R.layout.word_item, parent, false);
+        return new WordViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WordViewHolder holder, int position) {
+        WordModel wordModel = modelArrayList.get(position);
+        holder.button.setText(wordModel.word);
+        //search the word in google
+        holder.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String word = holder.button.getText().toString();
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.google.com/search?q=" + word));
+                v.getContext().startActivity(intent);
+            }
+        });
+    }
+
+
+
+
+    @Override
+    public int getItemCount() {
+        return modelArrayList.size();
+    }
+    public class WordViewHolder extends RecyclerView.ViewHolder {
+        Button button;
+        public WordViewHolder(@NonNull View itemView) {
+            super(itemView);
+            button = itemView.findViewById(R.id.button);
+        }
+    }
+}
